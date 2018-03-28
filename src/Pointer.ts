130214f09d0ae29ce8a79c0b495c482529cc1829
evilemon <slm_1990@126.com>
2018-03-28 18:14:26 +0800

    点击

---
+++ src/Pointer.ts
@@ -0,0 +1,136 @@
+import EventEmitter from './EventEmitter';
+
+declare const window: any;
+
+const postStartEvents: any = {
+    mousedown: ['mousemove', 'mouseup'],
+    touchstart: ['touchmove', 'touchend', 'touchcancel'],
+    pointerdown: ['pointermove', 'pointerup', 'pointercancel'],
+};
+
+export default class Pointer extends EventEmitter {
+    isPointerDown: boolean = false;
+    pointerIdentifier: any = null;
+    _boundPointerEvents: any[] = [];
+    handleEvent: any = null;
+    constructor() {
+        super();
+    }
+    bindStartEvent(el: Node) {
+        this._bindStartEvent(el, true);
+    }
+    unbindStartEvent(el: Node) {
+        this._bindStartEvent(el, false);
+    }
+    _bindStartEvent(el: Node, isAdd: boolean) {
+        this.handleEvent = this.handle();
+        const method = isAdd ? 'addEventListener' : 'removeEventListener';
+        let startEvent = 'mousedown';
+        // if ('PointerEvent' in window) {
+        //     startEvent = 'pointerdown';
+        // } else if ('ontouchstart' in window) {
+        //     startEvent = 'ontouchstart';
+        // }
+        if (isAdd) {
+            el.addEventListener(startEvent, this.handleEvent);
+        } else {
+            el.removeEventListener(startEvent, this.handleEvent);
+        }
+    }
+
+    handle() {
+        var self: any = this;
+        return function(event: any) {
+            let method = 'on' + event.type;
+            if (self[method]) {
+                self[method](event);
+            }
+        };
+    }
+
+    onmousemove(event: any) {
+        this._pointerMove(event, event);
+    }
+
+    _pointerMove(event: any, pointer: any) {
+        this.pointerMove(event, pointer);
+    }
+
+    pointerMove(event: any, pointer: any) {
+        this.emit('pointerMove', event, pointer);
+    }
+
+    onmouseup(event: any) {
+        this._pointerUp(event, event);
+    }
+
+    _pointerUp(event: any, pointer: any) {
+        this._pointerDone();
+        this.pointerUp(event, pointer);
+    }
+
+    pointerUp(event: any, pointer: any) {
+        this.emit('pointerUp', event, pointer);
+    };
+
+    _pointerDone() {
+        this._pointerReset();
+        this._unbindPostStartEvents();
+        this.pointerDone();
+    }
+
+    pointerDone() {
+        console.log('done')
+    }
+
+    _pointerReset() {
+        this.isPointerDown = false;
+        this.pointerIdentifier = null;
+    }
+
+    onmousedown(event: any) {
+        console.log(event.button);
+        const button = event.button;
+        if (button && (button !== 0 && button !== 1)) {
+            return;
+        }
+        this._pointerDown(event, event);
+    }
+
+    _pointerDown(event: any, pointer: any) {
+        // button = 0 is okay, 1-4 not  0 鼠标左键
+        if (event.button || this.isPointerDown) {
+            return;
+        }
+        this.isPointerDown = true;
+        this.pointerIdentifier = pointer.pointerId !== undefined ?
+            pointer.poiterId : pointer.identifier;
+        this.pointerDown(event, pointer);
+    }
+
+    pointerDown(event: any, pointer: any) {
+        this._bindPostStartEvent(event);
+        this.emit('pointerDown', event, pointer);
+    }
+
+    _bindPostStartEvent(event: any) {
+        if (!event) {
+            return;
+        }
+        const events = postStartEvents[event.type];
+        events.forEach(function (type: string) {
+            window.addEventListener(type, this.handleEvent);
+        }, this);
+        this._boundPointerEvents = events;
+    }
+
+    _unbindPostStartEvents = function () {
+        if (!this._boundPointerEvents) {
+            return;
+        }
+        this._boundPointerEvents.forEach(function (type: string) {
+            window.removeEventListener(type, this.handleEvent);
+        }, this);
+        this._boundPointerEvents = null;
+    };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import EventEmitter from './EventEmitter';
+import Pointer from './Pointer'
+
 
 declare var window: any;
 
 window.EventEmitter = EventEmitter;
+window.Pointer = Pointer;
